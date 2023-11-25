5584bb1510bf8065ac1e7881420fc37ccbda37f2
Chris <chris@nascent.xyz>
2023-11-25 10:30:56 -0500

    dead code

--- rust-enjoyer/src/lib.rs
+++ rust-enjoyer/src/lib.rs
@@ -138,75 +138,9 @@ pub fn function_selector_to_hex(x: u32) -> String {
     format!("0x{:0width$x}", x, width = std::mem::size_of::<u32>() * 2)
 }
 
-// playing around with AVX2
-pub fn theta_avx2(a: &mut [u64], b: &mut [u64; 5]) {
-    unsafe {
-        let b_0_3 = [
-            _mm256_set_epi64x(a[0] as i64, a[1] as i64, a[2] as i64, a[3] as i64),
-            _mm256_set_epi64x(a[5] as i64, a[6] as i64, a[7] as i64, a[8] as i64),
-            _mm256_set_epi64x(a[10] as i64, a[11] as i64, a[12] as i64, a[13] as i64),
-            _mm256_set_epi64x(a[15] as i64, a[16] as i64, a[17] as i64, a[18] as i64),
-            _mm256_set_epi64x(a[20] as i64, a[21] as i64, a[22] as i64, a[23] as i64),
-        ];
-
-        // reduce with _mm256_xor_si256
-        let b_0_3 = _mm256_xor_si256(
-            _mm256_xor_si256(
-                _mm256_xor_si256(b_0_3[0], b_0_3[1]),
-                _mm256_xor_si256(b_0_3[2], b_0_3[3]),
-            ),
-            b_0_3[4],
-        );
-
-        b[0] = _mm256_extract_epi64(b_0_3, 3) as u64;
-        b[1] = _mm256_extract_epi64(b_0_3, 2) as u64;
-        b[2] = _mm256_extract_epi64(b_0_3, 1) as u64;
-        b[3] = _mm256_extract_epi64(b_0_3, 0) as u64;
-        b[4] = a[4] ^ a[9] ^ a[14] ^ a[19] ^ a[24];
-
-        theta_(a, b, 4, 1, 0);
-        theta_(a, b, 0, 2, 1);
-        theta_(a, b, 1, 3, 2);
-        theta_(a, b, 2, 4, 3);
-        theta_(a, b, 3, 0, 4);
-    }
-}
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn test_theta_equivalence() {
-        let mut a = [0u64];
-        let mut b = [0u64; 5];
-
-        // Initialize `a` and `b` with some values
-        for i in 0..25 {
-            a[i] = i as u64;
-        }
-        for i in 0..5 {
-            b[i] = (i * 5) as u64;
-        }
-
-        let mut a_theta = a;
-        let mut b_theta = b;
-        theta(&mut a_theta, &mut b_theta);
-
-        #[cfg(target_feature = "avx2")]
-        {
-            let mut a_theta_avx2 = a;
-            let mut b_theta_avx2 = b;
-            theta_avx2(&mut a_theta_avx2, &mut b_theta_avx2);
-
-            assert_eq!(a_theta, a_theta_avx2);
-            assert_eq!(b_theta, b_theta_avx2);
-        }
-
-        #[cfg(not(target_feature = "avx2"))]
-        {
-            println!("AVX2 not enabled, skipping AVX2 test");
-        }
-    }
 
     #[test]
     fn test_shift() {
