5159712b2e125e8945b22a20d27230254d463572
Chris <chris@nascent.xyz>
2023-11-25 12:11:26 -0500

    cleanup benchmark

--- rust-enjoyer/benches/avx_benchmark.rs
+++ rust-enjoyer/benches/avx_benchmark.rs
@@ -1,16 +1,11 @@
-#![allow(unused)]
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use rust_enjoyer::sponges_avx::SpongeComputeSlice;
 
 fn bench_theta(c: &mut Criterion) {
     c.bench_function("theta", |b| {
         b.iter(|| {
-            let mut a = [0u64; 25];
-            let mut b = [0u64; 5];
-
-            // Initialize `a` and `b` with some values, e.g., for testing
-            for i in 0..25 {
-                a[i] = i as u64;
-            }
+            let mut a = [SpongeComputeSlice::default(); 25];
+            let mut b = [SpongeComputeSlice::default(); 5];
 
             black_box(rust_enjoyer::theta(&mut a, &mut b));
         })
@@ -26,5 +21,5 @@ fn bench_compute(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, bench_compute);
+criterion_group!(benches, bench_theta, bench_compute);
 criterion_main!(benches);
