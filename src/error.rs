59d564331d9a35665c86f8d9989eca91a3586979
supernovahs <harshitsinghal252@gmail.com>
2024-01-01 22:50:28 +0530

    clean up

--- src/error.rs
+++
@@ -1,3 +0,0 @@
-pub enum MiningError {
-    NumThreadsParse(String),
-}
--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,6 @@ pub mod sponges_avx;
 
 pub use small_string::*;
 pub use sponge::*;
-mod error;
-pub use error::MiningError;
 use std::ops::{BitAnd, BitOr, BitXor, BitXorAssign, Not, Shl, Shr};
 
 #[inline(always)]
